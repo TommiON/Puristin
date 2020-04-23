4d284be374dd11e95a3b1bbb0f12e27dfc459cee
Tommi Niittymies <tomminiittymies@Tommis-MacBook-Pro.local>
2020-04-23 15:47:12 +0300

    Added ResizingList class and tests

--- src/main/java/CustomDataStructures/ResizingList.java
+++ src/main/java/CustomDataStructures/ResizingList.java
@@ -4,23 +4,76 @@ package CustomDataStructures;
  * Implements an ArrayList-like data structure
  */
 public class ResizingList<T> {
-    private T[] contents;
-    private int size;
+    private int capacity = 10;
+    private T[] contents = (T[]) new Object[10];
+    private int lastSlotInUse = -1;
 
     /**
-     * Constructor, starts with capacity 10
+     * Adds new element at the end of the list
+     * @param element thing to be added
+     * @return always returns true; this non-obvious thing is to make the class compatible with Java's ArrayList
      */
-    public ResizingList() {
-        size = 10;
-        contents = (T[]) new Object[10];
+    public boolean add(T element) {
+        if (lastSlotInUse >= (capacity - 1)) { grow(); }
+        lastSlotInUse++;
+        contents[lastSlotInUse] = element;
+        return true;
+    }
+
+    public void add(int index, T element) {
+        if (index > lastSlotInUse) { return; }
+        if (lastSlotInUse >= (capacity - 1)) { grow(); }
+        lastSlotInUse++;
+        shiftRigth(index);
+        contents[index] = element;
+    }
+
+    public T get(int index) {
+        if (index > lastSlotInUse) {
+            return null;
+        }
+        return contents[index];
+    }
+
+    public T remove(int index) {
+        if (index > lastSlotInUse) { return null; }
+        T removedObject = contents[index];
+        shiftLeft(index);
+        lastSlotInUse--;
+        return removedObject;
+    }
+
+    public int size() { return lastSlotInUse + 1; }
+
+    @Override
+    public String toString() {
+        String stringRepresentation = "[";
+        for (int i = 0; i <= lastSlotInUse; i++) {
+            stringRepresentation = stringRepresentation + contents[i] + ", ";
+        }
+        stringRepresentation = stringRepresentation + "]";
+        return stringRepresentation;
     }
 
     private void grow() {
-        int largerSize = size + size/2;
+        int largerSize = capacity + (capacity / 2);
         T[] newContents = (T[]) new Object[largerSize];
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < capacity; i++) {
             newContents[i] = contents[i];
         }
         contents = newContents;
+        capacity = largerSize;
+    }
+
+    private void shiftLeft(int fromIndex) {
+        for (int i = fromIndex; i < lastSlotInUse; i++) {
+            contents[i] = contents[i + 1];
+        }
+    }
+
+    private void shiftRigth(int fromIndex) {
+        for (int i = lastSlotInUse; i > fromIndex; i--) {
+            contents[i] = contents[i - 1];
+        }
     }
 }
---
+++ src/test/java/CustomDataStructures/ResizingListTest.java
@@ -0,0 +1,98 @@
+package CustomDataStructures;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class ResizingListTest {
+    private ResizingList list;
+
+    @Before
+    public void init() {
+        list = new ResizingList();
+    }
+
+    @Test
+    public void initsCorrectly() {
+        assertEquals(0, list.size());
+        assertNull(list.get(0));
+        assertNull(list.get(1));
+    }
+
+    @Test
+    public void testSimpleAddingAndGettingWithStrings() {
+        list.add("a");
+        assertEquals(1, list.size());
+        list.add("b");
+        list.add("pidempi merkkijono");
+        assertEquals(3, list.size());
+        assertEquals("a", list.get(0));
+        assertEquals("b", list.get(1));
+        System.out.println(list.toString());
+    }
+
+    @Test
+    public void testGrowing() {
+        for (int i = 0; i < 1000; i++) {
+            list.add(String.valueOf(i));
+        }
+        assertEquals(1000, list.size());
+    }
+
+    @Test
+    public void testSimpleAddingWithDateObjects() {
+        list.add(new Date());
+        list.add(new Date());
+        assertEquals(2, list.size());
+        System.out.println(list.toString());
+    }
+
+    @Test
+    public void testAddingtoIndex() {
+        list.add("a");
+        list.add("b");
+        assertEquals(2, list.size());
+        assertEquals("a", list.get(0));
+        System.out.println(list.toString());
+        list.add(0, "c");
+        assertEquals(3, list.size());
+        assertEquals("c", list.get(0));
+        System.out.println(list.toString());
+    }
+
+    @Test
+    public void testRemoving() {
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        assertEquals(3, list.size());
+        assertEquals("b", list.get(1));
+        System.out.println(list.toString());
+        Object result = list.remove(1);
+        assertEquals("b", result.toString());
+        assertEquals(2, list.size());
+        assertEquals("c", list.get(1));
+        System.out.println(list.toString());
+    }
+
+    @Test
+    public void testGettingOutOfBounds() {
+        list.add("a");
+        assertEquals("a", list.get(0));
+        assertNull(list.get(1));
+    }
+
+    @Test
+    public void testAddingOutOfBounds() {
+        list.add(100, "a");
+        assertNull(list.get(100));
+    }
+
+    @Test
+    public void testRemovingOutOfBounds() {
+        assertNull(list.remove(100));
+    }
+}
